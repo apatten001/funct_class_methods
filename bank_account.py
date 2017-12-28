0939dc72a83f88053528d4d0b3b495513918c782
Arnold <apatten001@yahoo.com>
2017-12-28 16:43:13 -0500

    Add files via upload

--- bank_account.py
+++ bank_account.py
@@ -2,8 +2,9 @@
 
 class BankAccount():
 
-    def __init__(self):
+    def __init__(self,):
         self.balance = 0
+
 
     def deposit(self, amount):
         self.balance += amount
@@ -13,16 +14,25 @@ class BankAccount():
         self.balance -= amount
         return self.balance
 
+# here I am inheriting the BankAccount class to use info in the next class
 
 
+class MinimalBalance(BankAccount):
+
+    def __init__(self,minimal_balance):
+        BankAccount.__init__(self)
+        self.minimal_balance = minimal_balance
+
+    def withdraw(self,amount):
+
+        if self.balance - amount < self.minimal_balance:
+            print("Im sorry, you have insufficient funds!" + " " + " Your balance is: $" + str(self.balance))
+        else:
+            BankAccount.withdraw(self,amount)
 
 
+AJ = MinimalBalance(10)
 
-Arnold = BankAccount()
-Arnold.deposit(100)
-Arnold.withdraw(20)
-Arnold.deposit(10000)
-Arnold.withdraw(2000)
-print(Arnold.balance)
-
+AJ.deposit(70.23)
+AJ.withdraw(100)
 
